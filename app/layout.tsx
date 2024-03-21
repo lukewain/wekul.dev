a8f42dca4a864a6265b64c3ecf55e67de51f399f
lukewain <lukewainhouse@outlook.com>
2024-03-21 22:04:40 +0000

    Added buttons and looks fancy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
-import NavBar from "./components/navBar";
+// import NavBar from "./components/navBar";
 
 const inter = Poppins({ weight: '400', subsets: ["latin"] });
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,49 @@
 'use client';
 
 import Image from "next/image";
-import { useState } from "react";
-import Typewriter from "react-ts-typewriter";
+import React from "react";
+import { CodeBlock, atomOneDark, atomOneLight, nord, obsidian } from "react-code-blocks"
 import NavBar from "./components/navigation/navBar";
-import { motion } from "framer-motion";
+import { Button } from "@nextui-org/button"
 
 export default function Home() {
   return (
-  <main className="p-24">
-    <NavBar />
-    <div className="text-sm lg:flex">
-      {/* Create typewriter in text */}
-      <div id="header" className="justify-center lsitems-center ml-30 mt-10 mb-10">
-        <h1 id="main-grad" className="flex justify-center text-9xl font-bold">
-          wekul.dev
-        </h1>
+    <main className="p-24">
+      <div className="text-sm lg:flex">
+        <div className="flex ml-10 mt-10 mb-10">
+          <div className="flex items-center justify-center text-9xl font-bold w-full">
+            <h1 id="main-grad" className="text-10xl font-bold text-center">wekul.dev</h1>
+          </div>
+        </div>
       </div>
-    </div>
-    <div id="typewriterClass">
-      <h2 className="text-xl mt-10 mr-4">Hi, I'm Luke</h2>
-    </div>
-    {/* <div>
-      <h2 className="text-l mt-10 mr-4">I am 19 and do not know what I'm doing here</h2>
-    </div> */}
-  </main>
+
+      <hr className="mt-5 w-full" style={{
+        color: "white",
+        backgroundColor: "white",
+        opacity: "75%",
+        height: 3 // Adjust height as needed
+      }} />
+      <div className="h-[5rem]" />
+      <div className="mt-5 mx-auto w-2/5 content-evenly">
+        <CodeBlock
+          text={"const name: string = 'Luke';\nvar age: int = 19"}
+          language="javascript"
+          showLineNumbers={true}
+          theme={obsidian}
+        />
+      </div>
+      <div className="h-[2rem]" />
+      <div id="buttons" className="mt-10 ml-auto mr-auto mx-auto flex flex-wrap gap-7 w-1/2 justify-center text-2xl">
+        <Button color="danger" size="lg" variant="faded" style={{background: "#9099E4", borderRadius: "3px", padding: "6px"}} id="booton">
+          Home
+        </Button>
+        <Button color="primary" size="lg" variant="faded" style={{background: "#9099E4", borderRadius: "3px", padding: "6px"}} id="booton">
+          About
+        </Button>
+        <Button color="primary" size="lg" variant="bordered" style={{background: "#9099E4", borderRadius: "3px", padding: "6px"}} id="booton">
+          Contact
+        </Button>
+      </div>
+    </main>
   );
 }
